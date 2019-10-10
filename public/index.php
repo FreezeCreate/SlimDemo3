28289a3669e96d0ad1cfb94741d82170758102ad
FerreWagner <18408229270@163.com>
2019-10-10 17:20:32 +0800

    add-midware

--- public/index.php
+++ public/index.php
@@ -11,10 +11,18 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+//middware
+$app->add(function ($request, $response, $next){
+    $response->getBody()->write('BEFORE ');
+    $response = $next($request, $response); //下一层中间件
+    $response->getBody()->write(' AFTER');
+    $response->getBody()->write(' the next do');
+
+    return $response;
+});
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
-
     return $response;
 });
 $app->get('/', function () {
