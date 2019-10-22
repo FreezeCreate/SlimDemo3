8fe903859c53056e0bbb5efe9798c808e9c666bf
FerreWagner <18408229270@163.com>
2019-10-22 18:09:45 +0800

    use-Eloquent-only

--- app/controllers/MyController.php
+++ app/controllers/MyController.php
@@ -25,4 +25,23 @@ class MyController {
         //your code
         //to access items in the container... $this->ci->get('');
     }
+
+    public function getUser($request, $response, $args)
+    {
+        $capsule  = new \Illuminate\Database\Capsule\Manager();
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => '127.0.0.1',
+            'database'  => 'four_six',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]);
+        $capsule->setAsGlobal();
+        $conn  = $capsule;
+        $users = $conn::select('SELECT * FROM x2_user limit 10');
+        return $response->withJson($users, 200);
+    }
 }
